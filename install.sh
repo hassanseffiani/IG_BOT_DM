8130909f4b41586bd56b55786de2ac5d33563c95
hassanseffiani <hassanseffiani@gmail.com>
2022-03-09 17:20:21 +0100

    README

---
+++ install.sh
@@ -0,0 +1,8 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+source ~/.bashr
+nvm install node
+nvm install 14 # Specify major release only <br /> 
+nvm use 14
+sudo apt-get update
+sudo apt-get install python3.6
+sudo pip install -r /i/requirements.txt
